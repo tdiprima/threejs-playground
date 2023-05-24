2a7db1bea248cc82adb383a1ffdd7730099e3f7f
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-05-24 12:56:47 -0400

    ok

--- Code/Ales_Inferno/discipula.js
+++ Code/Ales_Inferno/discipula.js
@@ -6,28 +6,29 @@ const parentScene = new THREE.Scene();
 
 const fov = 45;
 const aspect = window.innerWidth / window.innerHeight;
+// If you do this, it makes the box flatter: aspect / 2.
 const near = 1;
 const far = 1000;
 
 // Create individual scenes and cameras
 const scene1 = new THREE.Scene();
-scene1.background = new THREE.Color("#ff0000");
-const camera1 = new THREE.PerspectiveCamera(fov, aspect / 2, near, far);
+scene1.background = new THREE.Color("#2b2b2b");
+const camera1 = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera1.position.set(0, 0, 10);
 
 const scene2 = new THREE.Scene();
-scene2.background = new THREE.Color("#ffff00");
-const camera2 = new THREE.PerspectiveCamera(fov, aspect / 2, near, far);
+scene2.background = new THREE.Color("#818181");
+const camera2 = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera2.position.set(0, 0, -10);
 
 const scene3 = new THREE.Scene();
-scene3.background = new THREE.Color("#00ffff");
-const camera3 = new THREE.PerspectiveCamera(fov, aspect / 2, near, far);
+scene3.background = new THREE.Color("#171717");
+const camera3 = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera3.position.set(10, 0, 0);
 
 const scene4 = new THREE.Scene();
-scene4.background = new THREE.Color("#0000ff");
-const camera4 = new THREE.PerspectiveCamera(fov, aspect / 2, near, far);
+scene4.background = new THREE.Color("#484848");
+const camera4 = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera4.position.set(-10, 0, 0);
 
 // Add scenes to the parent scene
@@ -83,6 +84,7 @@ function handleZoom(event) {
     controls2.dollyOut(event.deltaY);
     controls3.dollyOut(event.deltaY);
     controls4.dollyOut(event.deltaY);
+    // todo: controls1.dollyOut is not a function
   }
 }
 
@@ -92,33 +94,36 @@ document.addEventListener("wheel", handleZoom);
 // Function to toggle collective and individual zooming
 function toggleZoomMode() {
   collectiveZoom = !collectiveZoom;
+  console.log("collectiveZoom:", collectiveZoom);
 }
 
 // todo: Example usage: Press a button to toggle zooming mode
-// const zoomToggleButton = document.getElementById("zoom-toggle");
-// zoomToggleButton.addEventListener("click", toggleZoomMode);
+const zoomToggleButton = document.getElementById("zoom-toggle");
+zoomToggleButton.addEventListener("click", toggleZoomMode);
 
 function render() {
+  // Note: positioning is "flipped". Ce n'est pas ma faut. They did that. I saw it.
   const width = window.innerWidth / 2;
   const height = window.innerHeight / 2;
 
   renderer.setScissorTest(true);
 
-  // Start from the leftmost side (0) of the canvas, at the top (0) of the canvas.
+  // Top left
   renderer.setViewport(0, 0, width, height);
   renderer.setScissor(0, 0, width, height);
   renderer.render(scene1, camera1);
 
-  // Set the viewport to start from the middle of the canvas horizontally, at the top (0) of the canvas.
+  // Top right
   renderer.setViewport(width, 0, width, height);
   renderer.setScissor(width, 0, width, height);
   renderer.render(scene2, camera2);
 
+  // Bottom left
   renderer.setViewport(0, height, width, height);
   renderer.setScissor(0, height, width, height);
   renderer.render(scene3, camera3);
 
-  // Bottom-right quadrant. Start from the middle of the canvas horizontally and the middle of the canvas vertically.
+  // Bottom right
   renderer.setViewport(width, height, width, height);
   renderer.setScissor(width, height, width, height);
   renderer.render(scene4, camera4);
