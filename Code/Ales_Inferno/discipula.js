de0b6d1a3d8154575443aac5486ee628e2232878
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-05-25 18:22:07 -0400

    discipula

--- Code/Ales_Inferno/discipula.js
+++ Code/Ales_Inferno/discipula.js
@@ -2,118 +2,81 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
+const numScenes = 4;
 const parentScene = new THREE.Scene();
-
-const fov = 45;
-const aspect = window.innerWidth / window.innerHeight;
-// If you do this, it makes the box flatter: aspect / 2.
-const near = 1;
-const far = 1000;
-
-// Create individual scenes and cameras
-const scene1 = new THREE.Scene();
-scene1.background = new THREE.Color("#2b2b2b");
-const camera1 = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera1.position.set(0, 0, 10);
-
-const scene2 = new THREE.Scene();
-scene2.background = new THREE.Color("#818181");
-const camera2 = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera2.position.set(0, 0, -10);
-
-const scene3 = new THREE.Scene();
-scene3.background = new THREE.Color("#171717");
-const camera3 = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera3.position.set(10, 0, 0);
-
-const scene4 = new THREE.Scene();
-scene4.background = new THREE.Color("#484848");
-const camera4 = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera4.position.set(-10, 0, 0);
-
-// Add scenes to the parent scene
-parentScene.add(scene1);
-parentScene.add(scene2);
-parentScene.add(scene3);
-parentScene.add(scene4);
-
-let geometry1 = new THREE.BoxGeometry(1, 1, 1);
-let material1 = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-let cube1 = new THREE.Mesh(geometry1, material1);
-scene1.add(cube1);
-
-let geometry2 = new THREE.BoxGeometry(1, 1, 1);
-let material2 = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-let cube2 = new THREE.Mesh(geometry2, material2);
-scene2.add(cube2);
-
-let geometry3 = new THREE.BoxGeometry(1, 1, 1);
-let material3 = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-let cube3 = new THREE.Mesh(geometry3, material3);
-scene3.add(cube3);
-
-let geometry4 = new THREE.BoxGeometry(1, 1, 1);
-let material4 = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-let cube4 = new THREE.Mesh(geometry4, material4);
-scene4.add(cube4);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Create OrbitControls for each camera
-const controls1 = new OrbitControls(camera1, renderer.domElement);
-const controls2 = new OrbitControls(camera2, renderer.domElement);
-const controls3 = new OrbitControls(camera3, renderer.domElement);
-const controls4 = new OrbitControls(camera4, renderer.domElement);
-
 // Toggle variable to switch between collective and individual zooming
 let collectiveZoom = true;
 
-// function z(controls, event) {
-//   const movementX = event.movementX || event.touches[0].clientX - event.touches[1].clientX;
-//   const movementY = event.movementY || event.touches[0].clientY - event.touches[1].clientY;
-//   const distance = Math.sqrt(movementX * movementX + movementY * movementY);
-//   // Move the camera closer or farther away from its target
-//   controls.dolly(Math.exp(distance * -0.01));
-//   controls.pan(new THREE.Vector3(-movementX, movementY, 0));
-// }
+let scenes = [];
+let cameras = [];
+let controls = [];
+let meshes = [];
+
+let colors = [new THREE.Color("#2b2b2b"), new THREE.Color("#818181"), new THREE.Color("#171717"), new THREE.Color("#484848")]
+
+for (let i = 0; i < numScenes; i++) {
+  // Create individual scenes and cameras
+  const scene = new THREE.Scene();
+  scene.background = colors[i];
+  const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.set(0, 0, 10);
+
+  // Add scenes to the parent scene
+  parentScene.add(scene);
+
+  let geometry = new THREE.BoxGeometry(1, 1, 1);
+  let material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+  let cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  // Create OrbitControls for each camera
+  const control = new OrbitControls(camera, renderer.domElement);
+
+  scenes.push(scene);
+  cameras.push(camera);
+  controls.push(control);
+  meshes.push(cube);
+}
+
+// camera1.position.set(0, 0, 10);
+// camera2.position.set(0, 0, -10);
+// camera3.position.set(10, 0, 0);
+// camera4.position.set(-10, 0, 0);
+
+function z(control, event) {
+  const movementX = event.movementX || event.touches[0].clientX - event.touches[1].clientX;
+  const movementY = event.movementY || event.touches[0].clientY - event.touches[1].clientY;
+  const distance = Math.sqrt(movementX * movementX + movementY * movementY);
+
+  // Move the camera closer or farther away from its target
+  control.dolly(Math.exp(distance * -0.01));
+  control.pan(new THREE.Vector3(-movementX, movementY, 0));
+}
 
 // Function to handle zooming behavior
-/*
-function handleZoom(event) {
+function handleZoom1(event) {
   if (collectiveZoom) {
-    console.log("collective");
     // Zoom all cameras collectively
-    camera1.zoom += event.deltaY * 0.001;
-    camera2.zoom += event.deltaY * 0.001;
-    camera3.zoom += event.deltaY * 0.001;
-    camera4.zoom += event.deltaY * 0.001;
+    for (let i = 0; i < numScenes; i++) {
+      cameras[i].zoom += event.deltaY * 0.001;
+    }
+
   } else {
-    console.log("individual");
-    // TODO: IDK.
-    // Even if I comment out the other controls, it still moves all of them.
-    // And this is just (allegedly) affecting the zoom!  What about rotate; need to handle.
-
     // Zoom each camera separately
-    controls1.dollyOut(event.deltaY);
-    controls2.dollyOut(event.deltaY);
-    controls3.dollyOut(event.deltaY);
-    controls4.dollyOut(event.deltaY);
-
-    controls1.dollyIn(event.deltaX);
-    controls2.dollyIn(event.deltaX);
-    controls3.dollyIn(event.deltaX);
-    controls4.dollyIn(event.deltaX);
-
-    // I did delta like before...
-    // z(controls1, event)
-    // z(controls2, event)
-    // z(controls3, event)
-    // z(controls4, event)
+    for (let i = 0; i < numScenes; i++) {
+      // Yeah, this won't work...
+      controls[i].dollyOut(event.deltaY);
+      controls[i].dollyIn(event.deltaX);
+      // I did delta like before
+      // z(controls[i], event)
+    }
   }
 }
-*/
 
 let camera1Active = true;
 let camera2Active = false;
@@ -124,24 +87,23 @@ function handleZoom(event) {
   if (collectiveZoom) {
     console.log("collective");
     // Zoom all cameras collectively
-    camera1.zoom += event.deltaY * 0.001;
-    camera2.zoom += event.deltaY * 0.001;
-    camera3.zoom += event.deltaY * 0.001;
-    camera4.zoom += event.deltaY * 0.001;
+    for (let i = 0; i < numScenes; i++) {
+      cameras[i].zoom += event.deltaY * 0.001;
+    }
   } else {
     console.log("individual");
     if (camera1Active) {
-      controls1.dollyOut(event.deltaY);
-      controls1.dollyIn(event.deltaX);
+      cameras[0].dollyOut(event.deltaY);
+      cameras[0].dollyIn(event.deltaX);
     } else if (camera2Active) {
-      controls2.dollyOut(event.deltaY);
-      controls2.dollyIn(event.deltaX);
+      cameras[1].dollyOut(event.deltaY);
+      cameras[1].dollyIn(event.deltaX);
     } else if (camera3Active) {
-      controls3.dollyOut(event.deltaY);
-      controls3.dollyIn(event.deltaX);
+      cameras[2].dollyOut(event.deltaY);
+      cameras[2].dollyIn(event.deltaX);
     } else if (camera4Active) {
-      controls4.dollyOut(event.deltaY);
-      controls4.dollyIn(event.deltaX);
+      cameras[3].dollyOut(event.deltaY);
+      cameras[3].dollyIn(event.deltaX);
     }
   }
 }
@@ -169,29 +131,28 @@ function render() {
   // Top left
   renderer.setViewport(0, 0, width, height);
   renderer.setScissor(0, 0, width, height);
-  renderer.render(scene1, camera1);
+  renderer.render(scenes[0], cameras[0]);
 
   // Top right
   renderer.setViewport(width, 0, width, height);
   renderer.setScissor(width, 0, width, height);
-  renderer.render(scene2, camera2);
+  renderer.render(scenes[1], cameras[1]);
 
   // Bottom left
   renderer.setViewport(0, height, width, height);
   renderer.setScissor(0, height, width, height);
-  renderer.render(scene3, camera3);
+  renderer.render(scenes[2], cameras[2]);
 
   // Bottom right
   renderer.setViewport(width, height, width, height);
   renderer.setScissor(width, height, width, height);
-  renderer.render(scene4, camera4);
+  renderer.render(scenes[3], cameras[3]);
 }
 
 (function animLoop() {
   requestAnimationFrame(animLoop);
-  controls1.update();
-  controls2.update();
-  controls3.update();
-  controls4.update();
+  for (let i = 0; i < numScenes; i++) {
+    controls[i].update();
+  }
   render();
 })();
