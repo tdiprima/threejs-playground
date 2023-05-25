08ae5f0cdfe60be2d1a4988451302bb5ddc4762f
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-05-25 12:47:27 -0400

    Fixed! 4 images showing

--- Code/Ales_Inferno/esqueletos3.js
+++ Code/Ales_Inferno/esqueletos3.js
@@ -9,7 +9,7 @@ const height = window.innerHeight / 2;
 
 let scenes = [];
 let cameras = [];
-let meshes = [];
+// let meshes = [];
 let renderers = [];
 let controls = [];
 
@@ -18,14 +18,17 @@ for (let i = 0; i < numScenes; i++) {
   scenes.push(new THREE.Scene());
 
   // Create the cameras
-  cameras.push(new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000));
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+  camera.position.z = 10;
+  cameras.push(camera);
 
   // Create the materials, geometries, and meshes
   const image = loader.load(`image${i + 1}.jpg`);
   const material = new THREE.MeshBasicMaterial({ map: image, side: THREE.DoubleSide });
   const mesh = new THREE.Mesh(geometry, material);
-  meshes.push(mesh);
+  mesh.position.set(0, 0, 0)
   scenes[i].add(mesh);
+  // meshes.push(mesh);
 
   // Create the renderers
   const renderer = new THREE.WebGLRenderer();
@@ -42,27 +45,28 @@ for (let i = 0; i < numScenes; i++) {
   controls.push(control);
 }
 
-// Position the cameras
-cameras[0].position.set(0, 0, 2);
-cameras[1].position.set(0, 0, -2);
-cameras[2].position.set(2, 0, 0);
-cameras[3].position.set(-2, 0, 0);
+// Position the cameras - no.
+// cameras[0].position.set(0, 0, 2);
+// cameras[1].position.set(0, 0, -2);
+// cameras[2].position.set(2, 0, 0);
+// cameras[3].position.set(-2, 0, 0);
+
 // camera1.position.set(0, 0, 5);
 // const camera2 = camera1.clone();
 // const camera3 = camera1.clone();
 // const camera4 = camera1.clone();
 
-// Position the meshes
-meshes[0].position.set(-1, 1, 0);
-meshes[1].position.set(1, 1, 0);
-meshes[2].position.set(-1, -1, 0);
-meshes[3].position.set(1, -1, 0);
+// Position the meshes - no.
+// meshes[0].position.set(-1, 1, 0);
+// meshes[1].position.set(1, 1, 0);
+// meshes[2].position.set(-1, -1, 0);
+// meshes[3].position.set(1, -1, 0);
 // mesh1.position.set(-2, 2, 0);
 // mesh2.position.set(2, 2, 0);
 // mesh3.position.set(-2, -2, 0);
 // mesh4.position.set(2, -2, 0);
 
-// Position the renderers
+// Position the renderers - todo: is it wrong?
 renderers[0].domElement.style.left = "0px";
 renderers[0].domElement.style.top = "0px";
 renderers[1].domElement.style.left = `${width}px`;
@@ -72,8 +76,8 @@ renderers[2].domElement.style.top = `${height}px`;
 renderers[3].domElement.style.left = `${width}px`;
 renderers[3].domElement.style.top = `${height}px`;
 
+// todo: events or whatever
 
-// Render loop
 function render() {
   for (let i = 0; i < numScenes; i++) {
     renderers[i].render(scenes[i], cameras[i]);
