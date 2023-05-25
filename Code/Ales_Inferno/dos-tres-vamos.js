359b85985c343bdf511cfa508f02dd6bbfac6bf8
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-05-25 09:57:15 -0400

    2, 3, Go!

--- Code/Ales_Inferno/dos-tres-vamos.js
+++ Code/Ales_Inferno/dos-tres-vamos.js
@@ -1,39 +1,50 @@
 // "Parent scene and cameras.md"
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import {OrbitControls} from "three/addons/controls/OrbitControls.js";
 
-const fov = 45;
-const aspect = window.innerWidth / window.innerHeight;
-const near = 1;
-const far = 1000;
-
-// CAMERAS
-const camera1 = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera1.position.set(0, 0, 10);
-
-const camera2 = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera2.position.set(0, 0, -10);
-
-const camera3 = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera3.position.set(10, 0, 0);
-
-const camera4 = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera4.position.set(-10, 0, 0);
+const numScenes = 4;
 
 // RENDERER
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// CONTROLS
-const controls1 = new OrbitControls(camera1, renderer.domElement);
-const controls2 = new OrbitControls(camera2, renderer.domElement);
-const controls3 = new OrbitControls(camera3, renderer.domElement);
-const controls4 = new OrbitControls(camera4, renderer.domElement);
+// CAMERAS
+let cameras = [];
+for (let i = 0; i < numScenes; i++) {
+  cameras.push(new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000))
+}
 
-// ARRAYS
-let cameras = [camera1, camera2, camera3, camera4]; // Array of cameras
-let controls = [controls1, controls2, controls3, controls4]; // Array of controls
+// CAMERA POSITIONS
+cameras[0].position.set(0, 0, 10);
+cameras[1].position.set(0, 0, -10);
+cameras[2].position.set(10, 0, 0);
+cameras[3].position.set(-10, 0, 0);
+
+// CONTROLS
+let controls = []; // Array of controls
+for (let i = 0; i < numScenes; i++) {
+  let control = new OrbitControls(cameras[i], renderer.domElement);
+  control.name = `control${i + 1}`;
+  controls.push(control);
+}
+
+// SCENES
+const parentScene = new THREE.Scene();
+let scenes = [];
+for (let i = 0; i < numScenes; i++) {
+  let scene = new THREE.Scene();
+  parentScene.add(scene);
+  scenes.push(scene);
+}
+
+// CUBES
+for (let i = 0; i < numScenes; i++) {
+  let geometry = new THREE.BoxGeometry(1, 1, 1);
+  let material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
+  let cube = new THREE.Mesh(geometry, material);
+  scenes[i].add(cube);
+}
 
 // Initial active camera index
 let activeCameraIndex = 0;
@@ -41,7 +52,7 @@ let activeCameraIndex = 0;
 // Single instance of OrbitControls assigned to the active camera
 // let orbitControls = new OrbitControls(cameras[activeCameraIndex], renderer.domElement);
 
-let collectiveZoom = true;
+let collectiveZoom = false;
 
 function handleZoom(event) {
   if (collectiveZoom) {
@@ -54,8 +65,23 @@ function handleZoom(event) {
     console.log("individual");
     // let activeCamera = cameras[activeCameraIndex];
     let activeControls = controls[activeCameraIndex];
+    console.log(activeControls.name)
 
     activeControls.dollyOut(event.deltaY);
     activeControls.dollyIn(event.deltaX);
   }
 }
+
+function render() {
+  for (let i = 0; i < numScenes; i++) {
+    renderer.render(scenes[i], cameras[i]);
+  }
+}
+
+(function animLoop() {
+  requestAnimationFrame(animLoop);
+  for (let i = 0; i < numScenes; i++) {
+    controls[i].update();
+  }
+  render();
+})();
