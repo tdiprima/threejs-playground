ae8b8a167100a57147bfb2e4f0a8716693d8fc47
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-05-25 12:47:53 -0400

    camera and mesh position

--- Code/Ales_Inferno/esqueletos2.js
+++ Code/Ales_Inferno/esqueletos2.js
@@ -6,7 +6,7 @@ const loader = new THREE.TextureLoader();
 
 let scenes = [];
 let cameras = [];
-let meshes = [];
+// let meshes = [];
 let controls = [];
 
 // Set up the renderer
@@ -26,6 +26,7 @@ for (let i = 0; i < numScenes; i++) {
     0.1,
     1000
   );
+  camera.position.z = 10;
   cameras.push(camera);
 
   // Add the four images to the scenes
@@ -36,39 +37,31 @@ for (let i = 0; i < numScenes; i++) {
         side: THREE.DoubleSide
       })
   );
+  mesh.position.set(0, 0, 0)
   scene.add(mesh);
-  meshes.push(mesh);
+  // meshes.push(mesh);
 
   // Set up the controls for each camera
   const control = new OrbitControls(camera, renderer.domElement);
-  // todo: we hadn't done this until now
   control.target.set(mesh.position.x, mesh.position.y, mesh.position.z);
   control.enableZoom = true;
   control.enablePan = true;
   controls.push(control);
 }
 
-// Set up the initial camera positions
-cameras[0].position.set(0, 0, 10);
-cameras[1].position.set(0, 0, -10);
-cameras[2].position.set(10, 0, 10); // todo: huh?
-cameras[3].position.set(-10, 0, 10);
-// With this, nothing shows:
-// cameras[0].position.set(0, 0, 2);
-// cameras[1].position.set(0, 0, -2);
-// cameras[2].position.set(2, 0, 0);
-// cameras[3].position.set(-2, 0, 0);
+// Set up the initial camera positions - no.
+// cameras[0].position.set(0, 0, 10);
+// cameras[1].position.set(0, 0, -10);
+// cameras[2].position.set(10, 0, 10);
+// cameras[3].position.set(-10, 0, 10); // this is why the image appears slanted or at an angle
 
-// Position the meshes
+// Position the meshes - no. (btw 1's are better)
 // meshes[0].position.set(-2, 2, 0);
 // meshes[1].position.set(2, 2, 0);
 // meshes[2].position.set(-2, -2, 0);
 // meshes[3].position.set(2, -2, 0);
-// 1 is better:
-meshes[0].position.set(-1, 1, 0);
-meshes[1].position.set(1, 1, 0);
-meshes[2].position.set(-1, -1, 0);
-meshes[3].position.set(1, -1, 0);
+
+// todo: events or whatever
 
 function render() {
   // Add the scenes to the renderer
