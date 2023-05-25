5e933cbaec2a4f8727fcdf807e654fb4c6a391dd
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-05-25 11:50:19 -0400

    fix

--- Code/Ales_Inferno/esqueletos2.js
+++ Code/Ales_Inferno/esqueletos2.js
@@ -1,80 +1,87 @@
 import * as THREE from "three";
 import {OrbitControls} from "three/addons/controls/OrbitControls.js";
 
-// Set up the four scenes
-const scene1 = new THREE.Scene();
-const scene2 = new THREE.Scene();
-const scene3 = new THREE.Scene();
-const scene4 = new THREE.Scene();
+const numScenes = 4;
+const loader = new THREE.TextureLoader();
 
-// Create the cameras
-const camera1 = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-const camera2 = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-const camera3 = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-const camera4 = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+let scenes = [];
+let cameras = [];
+let meshes = [];
+let controls = [];
 
 // Set up the renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Add the four images to the scenes
-const loader = new THREE.TextureLoader();
-const geometry = new THREE.PlaneGeometry(5, 5);
+for (let i = 0; i < numScenes; i++) {
+  // Set up the four scenes
+  const scene = new THREE.Scene();
+  scenes.push(scene);
 
-const image1 = loader.load('image1.jpg');
-const material1 = new THREE.MeshBasicMaterial({ map: image1 })
-const mesh1 = new THREE.Mesh(geometry, material1);
-mesh1.position.set(-2, 2, 0);
-scene1.add(mesh1);
+  // Create the cameras
+  const camera = new THREE.PerspectiveCamera(
+    50,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  cameras.push(camera);
 
-const image2 = loader.load('image2.jpg');
-const material2 = new THREE.MeshBasicMaterial({ map: image2 })
-const mesh2 = new THREE.Mesh(geometry, material2);
-mesh2.position.set(2, 2, 0);
-scene2.add(mesh2);
+  // Add the four images to the scenes
+  const mesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(5, 5),
+    new THREE.MeshBasicMaterial({
+        map: loader.load(`image${i + 1}.jpg`),
+        side: THREE.DoubleSide
+      })
+  );
+  scene.add(mesh);
+  meshes.push(mesh);
 
-const image3 = loader.load('image3.jpg');
-const material3 = new THREE.MeshBasicMaterial({ map: image3 })
-const mesh3 = new THREE.Mesh(geometry, material3);
-mesh3.position.set(-2, -2, 0);
-scene3.add(mesh3);
-
-const image4 = loader.load('image4.jpg');
-const material4 = new THREE.MeshBasicMaterial({ map: image4 })
-const mesh4 = new THREE.Mesh(geometry, material4);
-mesh4.position.set(2, -2, 0);
-scene4.add(mesh4);
-
-// Set up the controls for each camera
-const controls1 = new OrbitControls(camera1, renderer.domElement);
-controls1.target.set(mesh1.position.x, mesh1.position.y, mesh1.position.z);
-controls1.enableZoom = true;
-controls1.enablePan = true;
-
-const controls2 = new OrbitControls(camera2, renderer.domElement);
-controls2.target.set(mesh2.position.x, mesh2.position.y, mesh2.position.z);
-controls2.enableZoom = true;
-controls2.enablePan = true;
-
-const controls3 = new OrbitControls(camera3, renderer.domElement);
-controls3.target.set(mesh3.position.x, mesh3.position.y, mesh3.position.z);
-controls3.enableZoom = true;
-controls3.enablePan = true;
-
-const controls4 = new OrbitControls(camera4, renderer.domElement);
-controls4.target.set(mesh4.position.x, mesh4.position.y, mesh4.position.z);
-controls4.enableZoom = true;
-controls4.enablePan = true;
+  // Set up the controls for each camera
+  const control = new OrbitControls(camera, renderer.domElement);
+  // todo: we hadn't done this until now
+  control.target.set(mesh.position.x, mesh.position.y, mesh.position.z);
+  control.enableZoom = true;
+  control.enablePan = true;
+  controls.push(control);
+}
 
 // Set up the initial camera positions
-camera1.position.set(0, 0, 10);
-camera2.position.set(0, 0, 10);
-camera3.position.set(0, 0, 10);
-camera4.position.set(0, 0, 10);
+cameras[0].position.set(0, 0, 10);
+cameras[1].position.set(0, 0, -10);
+cameras[2].position.set(10, 0, 10); // todo: huh?
+cameras[3].position.set(-10, 0, 10);
+// With this, nothing shows:
+// cameras[0].position.set(0, 0, 2);
+// cameras[1].position.set(0, 0, -2);
+// cameras[2].position.set(2, 0, 0);
+// cameras[3].position.set(-2, 0, 0);
 
-// Add the scenes to the renderer
-renderer.render(scene1, camera1);
-renderer.render(scene2, camera2);
-renderer.render(scene3, camera3);
-renderer.render(scene4, camera4); // cut off here
+// Position the meshes
+// meshes[0].position.set(-2, 2, 0);
+// meshes[1].position.set(2, 2, 0);
+// meshes[2].position.set(-2, -2, 0);
+// meshes[3].position.set(2, -2, 0);
+// 1 is better:
+meshes[0].position.set(-1, 1, 0);
+meshes[1].position.set(1, 1, 0);
+meshes[2].position.set(-1, -1, 0);
+meshes[3].position.set(1, -1, 0);
+
+function render() {
+  // Add the scenes to the renderer
+  for (let i = 0; i < numScenes; i++) {
+    renderer.render(scenes[i], cameras[i]);
+  }
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  for (let i = 0; i < numScenes; i++) {
+    controls[i].update();
+  }
+  render();
+}
+animate();
--- Code/Ales_Inferno/esqueletos3.js
+++ Code/Ales_Inferno/esqueletos3.js
@@ -1,101 +1,90 @@
 import * as THREE from "three";
 import {OrbitControls} from "three/addons/controls/OrbitControls.js";
 
-// Create the scenes
-const scene1 = new THREE.Scene();
-const scene2 = new THREE.Scene();
-const scene3 = new THREE.Scene();
-const scene4 = new THREE.Scene();
-
-// Create the cameras
-const camera1 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera1.position.set(0, 0, 5);
-const camera2 = camera1.clone();
-const camera3 = camera1.clone();
-const camera4 = camera1.clone();
-
-// Create the geometries
-const geometry = new THREE.PlaneGeometry(1, 1);
-
-// Create the materials and meshes
+const numScenes = 4;
 const loader = new THREE.TextureLoader();
-const image1 = loader.load('image1.jpg');
-const material1 = new THREE.MeshBasicMaterial({ map: image1 });
-const mesh1 = new THREE.Mesh(geometry, material1);
-mesh1.position.set(-2, 2, 0);
-scene1.add(mesh1);
-
-const image2 = loader.load('image2.jpg');
-const material2 = new THREE.MeshBasicMaterial({ map: image2 });
-const mesh2 = new THREE.Mesh(geometry, material2);
-mesh2.position.set(2, 2, 0);
-scene2.add(mesh2);
-
-const image3 = loader.load('image3.jpg');
-const material3 = new THREE.MeshBasicMaterial({ map: image3 });
-const mesh3 = new THREE.Mesh(geometry, material3);
-mesh3.position.set(-2, -2, 0);
-scene3.add(mesh3);
-
-const image4 = loader.load('image4.jpg');
-const material4 = new THREE.MeshBasicMaterial({ map: image4 });
-const mesh4 = new THREE.Mesh(geometry, material4);
-mesh4.position.set(2, -2, 0);
-scene4.add(mesh4);
-
-// Create the renderers
+const geometry = new THREE.PlaneGeometry(1, 1);
 const width = window.innerWidth / 2;
 const height = window.innerHeight / 2;
 
-const renderer1 = new THREE.WebGLRenderer();
-renderer1.setSize(width, height);
-document.body.appendChild(renderer1.domElement);
+let scenes = [];
+let cameras = [];
+let meshes = [];
+let renderers = [];
+let controls = [];
 
-const renderer2 = new THREE.WebGLRenderer();
-renderer2.setSize(width, height);
-renderer2.domElement.style.position = 'absolute';
-renderer2.domElement.style.top = "0px";
-renderer2.domElement.style.left = `${width}px`;
+for (let i = 0; i < numScenes; i++) {
+  // Create the scenes
+  scenes.push(new THREE.Scene());
 
-document.body.appendChild(renderer2.domElement);
-const renderer3 = new THREE.WebGLRenderer();
-renderer3.setSize(width, height);
-renderer3.domElement.style.position = 'absolute';
-renderer3.domElement.style.top = `${height}px`;
-document.body.appendChild(renderer3.domElement);
+  // Create the cameras
+  cameras.push(new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000));
 
-const renderer4 = new THREE.WebGLRenderer();
-renderer4.setSize(width, height);
-renderer4.domElement.style.position = 'absolute';
-renderer4.domElement.style.left = `${width}px`;
-renderer4.domElement.style.top = `${height}px`;
-document.body.appendChild(renderer4.domElement);
+  // Create the materials, geometries, and meshes
+  const image = loader.load(`image${i + 1}.jpg`);
+  const material = new THREE.MeshBasicMaterial({ map: image, side: THREE.DoubleSide });
+  const mesh = new THREE.Mesh(geometry, material);
+  meshes.push(mesh);
+  scenes[i].add(mesh);
 
-// Set up event listeners to control the cameras
-const controls1 = new OrbitControls(camera1, renderer1.domElement);
-const controls2 = new OrbitControls(camera2, renderer2.domElement);
-const controls3 = new OrbitControls(camera3, renderer3.domElement);
-const controls4 = new OrbitControls(camera4, renderer4.domElement);
+  // Create the renderers
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(width, height);
+  if (i > 0) {
+    renderer.domElement.style.position = 'absolute';
+  }
+  document.body.appendChild(renderer.domElement);
+  renderers.push(renderer);
 
-controls1.addEventListener('change', render);
-controls2.addEventListener('change', render);
-controls3.addEventListener('change', render);
-controls4.addEventListener('change', render);
+  // Set up event listeners to control the cameras
+  const control = new OrbitControls(cameras[i], renderers[i].domElement);
+  // control.addEventListener('change', render);
+  controls.push(control);
+}
+
+// Position the cameras
+cameras[0].position.set(0, 0, 2);
+cameras[1].position.set(0, 0, -2);
+cameras[2].position.set(2, 0, 0);
+cameras[3].position.set(-2, 0, 0);
+// camera1.position.set(0, 0, 5);
+// const camera2 = camera1.clone();
+// const camera3 = camera1.clone();
+// const camera4 = camera1.clone();
+
+// Position the meshes
+meshes[0].position.set(-1, 1, 0);
+meshes[1].position.set(1, 1, 0);
+meshes[2].position.set(-1, -1, 0);
+meshes[3].position.set(1, -1, 0);
+// mesh1.position.set(-2, 2, 0);
+// mesh2.position.set(2, 2, 0);
+// mesh3.position.set(-2, -2, 0);
+// mesh4.position.set(2, -2, 0);
+
+// Position the renderers
+renderers[0].domElement.style.left = "0px";
+renderers[0].domElement.style.top = "0px";
+renderers[1].domElement.style.left = `${width}px`;
+renderers[1].domElement.style.top = "0px";
+renderers[2].domElement.style.left = "0px";
+renderers[2].domElement.style.top = `${height}px`;
+renderers[3].domElement.style.left = `${width}px`;
+renderers[3].domElement.style.top = `${height}px`;
+
 
 // Render loop
 function render() {
-  renderer1.render(scene1, camera1);
-  renderer2.render(scene2, camera2);
-  renderer3.render(scene3, camera3);
-  renderer4.render(scene4, camera4);
+  for (let i = 0; i < numScenes; i++) {
+    renderers[i].render(scenes[i], cameras[i]);
+  }
 }
 
 function animate() {
   requestAnimationFrame(animate);
-  controls1.update();
-  controls2.update();
-  controls3.update();
-  controls4.update();
+  for (let i = 0; i < numScenes; i++) {
+    controls[i].update();
+  }
   render();
 }
 
