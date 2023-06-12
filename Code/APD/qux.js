0dcc47a9c426b1ffae061597fd2d7f5eda14607e
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-06-12 16:40:43 -0400

    ok

--- Code/APD/qux.js
+++
@@ -1,3 +0,0 @@
-// Fixes the document thing, but doesn't fix always saying "image 1"
-renderer1.domElement.addEventListener('mousedown', onDocumentMouseDown);
-renderer2.domElement.addEventListener('mousedown', onDocumentMouseDown);
