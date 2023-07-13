8494c1b6fa8e5b8e07a50c0e54bf9936580c23ed
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-07-13 11:36:38 -0400

    dumpObject

---
+++ Code/misc/dumpObject.js
@@ -0,0 +1,13 @@
+function dumpObject(obj, lines = [], isLast = true, prefix = '') {
+  const localPrefix = isLast ? '└─' : '├─';
+  lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`);
+  const newPrefix = prefix + (isLast ? '  ' : '│ ');
+  const lastNdx = obj.children.length - 1;
+  obj.children.forEach((child, ndx) => {
+    const isLast = ndx === lastNdx;
+    dumpObject(child, lines, isLast, newPrefix);
+  });
+  return lines;
+}
+
+// console.log(`%c${dumpObject(scene).join('\n')}`, "color: #00ff00;");
