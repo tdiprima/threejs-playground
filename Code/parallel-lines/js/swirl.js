79104409d10dfe4a868ba8a28a6aacb5f48617a8
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-09-28 09:57:48 -0400

    spiral

--- Code/parallel-lines/js/swirl.js
+++ Code/parallel-lines/js/swirl.js
@@ -3,18 +3,21 @@
 // SCENE, CAMERA, RENDERER
 // -------- ----------
 let scene = new THREE.Scene();
+// 45-degree field of view, aspect ratio of 4/3, and near and far clipping planes of 0.5 and 100
 let camera = new THREE.PerspectiveCamera(45, 4 / 3, 0.5, 100);
 camera.position.set(10, 10, 10);
 camera.lookAt(0, 0, 0);
 
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(640, 480, false);
+// Append the renderer's DOM element to an HTML element with the ID of "demo"
+// or to the document's body if no such element exists.
 (document.getElementById("demo") || document.body).appendChild(renderer.domElement);
 
 // -------- ----------
 // HELPERS
 // -------- ----------
-// create points helper
+// Generate an array of 3D points that form a spiral
 let createPoints = function(len, rotationCount = 8, height = 5, maxRadius = 5) {
   let yDelta = height / len;
   let points = [];
@@ -42,7 +45,7 @@ let createPoints = function(len, rotationCount = 8, height = 5, maxRadius = 5) {
   return points;
 };
 
-// update lines group
+// Update a group of line geometries by setting their points to form spirals
 let updateLinesGroup = function(lines, rs, rDelta, height, radius) {
   lines.children.forEach((line, i, arr) => {
     let per = (i + 1) / arr.length;
@@ -56,11 +59,13 @@ let updateLinesGroup = function(lines, rs, rDelta, height, radius) {
 // create lines group
 let lines = new THREE.Group();
 let lineCount = 12;
+// green, red, blue, magenta, cyan, and yellow
 let colors = [0x00ff00, 0xff0000, 0x0000ff, 0xff00ff, 0x00ffff, 0xffff00];
 let i = 0;
 
 while (i < lineCount) {
   let per = i / lineCount;
+  // Generate an array of points for each line to form a spiral
   let points = createPoints(100, 1 + 0.2 * per, 0, 5);
 
   // 1D array, { x: 0, y: 0, z: 0 } objects, length 100
@@ -75,15 +80,19 @@ while (i < lineCount) {
       linewidth: 6
     })
   ));
+  // Add line to group of lines
   lines.add(line);
   i += 1;
 }
 
 scene.add(lines);
+// Update the group of lines with spirals that have different properties
 updateLinesGroup(lines, 0.5, 1.4, 10, 4);
+// Adjust position of lines group
 lines.position.y = -8;
 
 // -------- ----------
 // RENDER
 // -------- ----------
+// Note: there is no render loop to continuously update and render the scene
 renderer.render(scene, camera);
